91b6076297bd9d2acd3ba8250f8211cca52c2b6b
Marco Kühbauch <marcokuehbauch@gmail.com>
2020-09-04 10:08:42 +0200

    configure ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,21 +14,6 @@ module.exports = {
       options: {
         // The property ID; the tracking code won't be generated without it
         trackingId: process.env.GA_TRACKING_ID,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
       },
     },
     `gatsby-plugin-react-helmet`,
