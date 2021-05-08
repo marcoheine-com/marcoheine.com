3c69a71577fe7e23b7e5f59026189da78348311f
Marco Kühbauch <marcokuehbauch@gmail.com>
2021-05-08 13:09:07 +0200

    add prop based styling

--- src/components/main/ui.js
+++ src/components/main/ui.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   flex-grow: 1;
-  max-width: maxWidth;
+  max-width: ${({ maxWidth }) => maxWidth}};
   margin-left: auto;
   margin-right: auto;
   padding: 50px 20px 160px 20px;
