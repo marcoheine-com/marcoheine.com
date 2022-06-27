187b1482c61bebc9fae24069dba8edb2e6cc7c92
Marco Kühbauch <marcokuehbauch@gmail.com>
2022-06-27 12:04:54 +0200

    add blog rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,66 @@ module.exports = {
     siteUrl: `https://marcoheine.com`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(({ node }) => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ 'content:encoded': node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(
+                  filter: { fields: { type: { eq: "blog-post" } } }
+                  sort: { order: DESC, fields: [frontmatter___date] }
+                  limit: 1000
+                ) {
+                  edges {
+                    node {
+                      fields {
+                        slug
+                      }
+                      frontmatter {
+                        title
+                      }
+                      excerpt
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: "Marco Heine's blog RSS Feed",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: '^/blog/',
+          },
+        ],
+      },
+    },
     'gatsby-plugin-postcss',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sitemap`,
