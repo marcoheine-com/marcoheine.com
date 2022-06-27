54fae5e96dbae26363bb4d705a7549786c8f6943
Marco Kühbauch <marcokuehbauch@gmail.com>
2022-06-27 12:59:03 +0200

    change url of rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,8 +32,8 @@ module.exports = {
                 return Object.assign({}, node.frontmatter, {
                   description: node.excerpt,
                   date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  url: `${site.siteMetadata.siteUrl}/${node.fields.slug}`,
+                  guid: `${site.siteMetadata.siteUrl}/${node.fields.slug}`,
                   custom_elements: [{ 'content:encoded': node.html }],
                 })
               })
@@ -61,11 +61,6 @@ module.exports = {
             `,
             output: '/rss.xml',
             title: "Marco Heine's blog RSS Feed",
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: '^/blog/',
           },
         ],
       },
