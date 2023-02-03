53ea8aa8a69d1e028c1acbe963cd3778e388ee87
Marco Heine <marco@marcoheine.com>
2023-02-03 12:59:34 +0100

    turn every til post into mdx

--- lib/til.ts
+++ lib/til.ts
@@ -29,8 +29,8 @@ type Options = {
 const postsDirectory = join(process.cwd(), 'today-i-learned')
 
 export function getTILPostBySlug(slug: string) {
-  const realSlug = slug.replace(/\.md$/, '')
-  const fullPath = join(postsDirectory, `${realSlug}.md`)
+  const realSlug = slug.replace(/\.mdx$/, '')
+  const fullPath = join(postsDirectory, `${realSlug}.mdx`)
 
   const fileContents = fs.readFileSync(fullPath, 'utf8')
   const { data, content } = matter(fileContents)
