f601474871183c154740f73a5b5c4a6ce8710c62
Marco Heine <marco@marcoheine.com>
2023-02-08 08:16:04 +0100

    add today-i-learned redirects

--- next.config.js
+++ next.config.js
@@ -19,6 +19,90 @@ const nextConfig = {
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   i18n,
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/today-i-learned/javascript/',
+        destination: '/today-i-learned/category/javascript/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/react/',
+        destination: '/today-i-learned/category/react/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/api/',
+        destination: '/today-i-learned/category/api/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/accessibility/',
+        destination: '/today-i-learned/category/accessibility/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/backend/',
+        destination: '/today-i-learned/category/backend/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/css/',
+        destination: '/today-i-learned/category/css/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/gatsbyjs/',
+        destination: '/today-i-learned/category/gatsbyjs/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/git/',
+        destination: '/today-i-learned/category/git/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/html/',
+        destination: '/today-i-learned/category/html/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/performance/',
+        destination: '/today-i-learned/category/performance/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/prisma/',
+        destination: '/today-i-learned/category/prisma/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/redux/',
+        destination: '/today-i-learned/category/redux/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/software development/',
+        destination: '/today-i-learned/category/software-development/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/testing/',
+        destination: '/today-i-learned/category/testing/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/ux/',
+        destination: '/today-i-learned/category/ux/',
+        permanent: true,
+      },
+      {
+        source: '/today-i-learned/web-security/',
+        destination: '/today-i-learned/category/web-security/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 // Merge MDX config with Next.js config
